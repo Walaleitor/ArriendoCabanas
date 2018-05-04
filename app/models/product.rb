cd0dc4eaeca0acd5349097b4b291c8fb7ebb24cd
Matias Robledo <matias.robledo@ceinf.cl>
2018-05-03 23:54:25 -0300

    Se agregan validaciones para Productos(product)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,21 @@
 class Product < ApplicationRecord
 	has_many :payments
 	belongs_to :cabin
+
+	#Validaciones
+	validates :name,
+			  presence: true,
+			  lenght: {minimun: 3, maximum: 20 }
+
+	validates :description,
+			  presence: true,
+			  lenght: {minimun: 3, maximum: 40 }
+
+	validates :quantity,
+			  presence: true,
+			  numericality: true
+
+	validates :price,
+			  presence: true,
+			  numericality: true
 end
