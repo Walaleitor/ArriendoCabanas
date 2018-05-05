c18737b2c40d52b9fe588afdcc9520c7e90bea4a
Matias Robledo <matias.robledo@ceinf.cl>
2018-05-04 23:40:11 -0300

    Se agregan validaciones para Bicicletas(Bycicle)

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,4 +1,22 @@
 class Bicycle < ApplicationRecord
 	belongs_to :cabin
 	has_many :payments
+
+	#Validaciones
+	validates :model,
+			  presence: true,
+			  lenght: {minimun: 3, maximum: 20 }
+
+    validates :ring_size,
+    		  presence: true,
+    		  numericality: true
+
+    validates :tipo,
+    		  presence: true,
+    		  lenght: {minimun: 3, maximum: 20 }
+
+    validates :value,
+    		  presence: true,
+    		  numericality: true
+
 end
