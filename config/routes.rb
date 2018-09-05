9ed86ee8f411d690f3c73d279d24483a88ad6c69
Matias Robledo <matias.robledo@ceinf.cl>
2018-09-05 15:41:58 -0300

    Se corrigen vistas de inicio de Sesión además se agrega inicio de sesión como root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
+devise_scope :user do
+  authenticated :user do
+    root 'cabins#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'devise/sessions#new', as: :unauthenticated_root
+  end
+end
   root 'cabins#index'
   get 'cabins/:id/index', to: 'reservations#index'
   get 'reservations', to: 'reservations#index', as: 'reservations'
