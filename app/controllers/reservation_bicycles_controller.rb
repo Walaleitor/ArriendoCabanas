788d174650a4f7048f884d5529d342ace6c99be3
Sebastian Salazar Pulgar <sebanicosp@gmail.com>
2018-09-10 15:15:08 -0300

    se agregar contenido a add_bicycles a reserevatiopn

--- app/controllers/reservation_bicycles_controller.rb
+++ app/controllers/reservation_bicycles_controller.rb
@@ -2,6 +2,9 @@ class ReservationBicyclesController < ApplicationController
 
     def add
         @cabin = Cabin.find(params[:cabin_id])
-        @reservation = @cabin.reservations.find(params[:id])        
+        @reservation = @cabin.reservations.find(params[:id])
+        @bicycles = @cabin.bicycles.all
+        @payments = @reservation.bicycle_payments.all
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-devise_scope :user do
-  authenticated :user do
-    root 'cabins#index', as: :authenticated_root
+    devise_scope :user do
+      authenticated :user do
+      root 'cabins#index', as: :authenticated_root
 
-  end
+    end
 
-  unauthenticated do
-    root 'devise/sessions#new', as: :unauthenticated_root
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
   end
-end
   root 'cabins#index'
   get 'cabins/:id/index', to: 'reservations#index'
   get 'reservations', to: 'reservations#index', as: 'reservations'
@@ -19,7 +19,7 @@ end
   get 'cabins/:id/market', to: 'markets#show', as: 'market'
   get 'cabins/:cabin_id/reservations/:id/add_product', to: 'reservation_products#add', as:'reservation_products'
   get 'cabins/:cabin_id/reservations/:id/add_services', to: 'reservation_services#add', as:'reservation_services'
-  get 'cabins/:cabin_id/reservations/:id/add_bicycle', to: 'reservation_bicycle#add', as:'reservation_bicycles'
+  get 'cabins/:cabin_id/reservations/:id/add_bicycle', to: 'reservation_bicycles#add', as:'reservation_bicycles'
 
   resources :customers
   resources :cabins do
